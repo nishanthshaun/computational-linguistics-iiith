d32adda55d8e6730a657da3210d9537655ed11cb
nishanthshaun <nishanthbaladari@gmail.com>
2020-07-18 19:53:09 +0530

    background values

--- RDIP/Codes/Exp-4.js
+++ RDIP/Codes/Exp-4.js
@@ -1,24 +1,36 @@
+var corpus1 = ["A mouse was having a very bad time.She could find no food at all.She looked here and there,but there was no food,and she grew very thin.", "At last the mouse found a basket,full of corn.There was a small hole in the basket,and she crept in.She could just get through the hole.Then she began to eat the corn.", "Being very hungry,she ate a great deal , and went on eating and eating.She had grown very fat before she felt that she had had enough.", "When the mouse tried to climb out of the basket,she could not . She was too fat to pass through the hole.\"How shall I climb out?\"said the mouse.", " \"Oh,how shall I climb out?\"Just then a rat came along,and he heard the mouse. \"Mouse\" said the rat,\" If you want to climb out of the basket,you must wait till you have grown as thin as you were when you went in."]
+
+var corpus2 = ["A wolf carried off a lamb. The lamb said,\" I know you are going to eat me, but before you eat me I would like to hear you play the flute.", "I have heard that you can play the flute better than anyone else, even the shepherd himself.", "The wolf was so pleased at this that he took out his flute and began to play.", "When he had done, the lamb insisted him to play once more and the wolf played again.", "The shepherd and the dogs heard the sound, and they came running up and fell on the wolf and the lamb was able to get back to the flock."]
+
+var corpus3 = ["A man had a little dog, and he was very fond of it. He would pat its head, and take it on his knee, and talk to it.", "Then he would give it little bits of food from his own plate. A donkey looked in at the window and saw the man and the dog. \"Why does he not make a pet of me?\" said the donkey.", "\"It is not fair. I work hard, and the dog only wags its tail, and barks, and jumps on its master's knee. It is not fair.\" Then the donkey said to himself,", "\"If I do what the dog does, he may make a pet of me.\" So the donkey ran into the room. It brayed as loudly as it could. It wagged its tail so hard that it knocked over a jar on the table.", "Then it tried to jump on to its master's knee. The master thought the donkey was mad, and he shouted, \"Help! Help!\" Men came running in with sticks, and they beat the donkey till it ran out of the house,", "and they drove it back to the field. \"I only did what the dog does,\" said the donkey,\" and yet they make a pet of the dog, and they beat me with sticks. It is not fair.\""]
+var res = "";
 function corpusfunc()
 {
 var x = document.getElementById("corpus").value;
 if(x=="corpus1")
 {
     document.getElementById("demo").innerHTML = "";
+    document.getElementById("demo1").innerHTML = "";
     document.getElementById("table1").innerHTML = "";
+    document.getElementById("sub").innerHTML = "";
     var a = corpus1func();
     return a;
 }
 else if(x=="corpus2")
 {
     document.getElementById("demo").innerHTML = "";
+    document.getElementById("demo1").innerHTML = "";
     document.getElementById("table1").innerHTML = "";
+    document.getElementById("sub").innerHTML = "";
     var b = corpus2func();
     return b;
 }
 else if(x=="corpus3")
 {
     document.getElementById("demo").innerHTML = "";
+    document.getElementById("demo1").innerHTML = "";
     document.getElementById("table1").innerHTML = "";
+    document.getElementById("sub").innerHTML = "";
     var c = corpus3func();
     return c;
 }
@@ -28,17 +40,18 @@ else
     document.getElementById("demo").innerHTML = "";
     document.getElementById("demo1").innerHTML = "";
     document.getElementById("table1").innerHTML = "";
+    document.getElementById("sub").innerHTML = "";
     return false;
 }
 }
 function corpus1function()
 {
-    var corpus1 = ["A mouse was having a very bad time. She could find no food at all. She looked here and there, but there was no food, and she grew very thin.","At last the mouse found a basket, full of corn. There was a small hole in the basket, and she crept in. She could just get through the hole. Then she began to eat the corn.","Being very hungry, she ate a great deal, and went on eating and eating. She had grown very fat before she felt that she had had enough.","When the mouse tried to climb out of the basket, she could not. She was too fat to pass through the hole. \"How shall I climb out?\" said the mouse.","\"Oh, how shall I climb out?\" Just then a rat came along, and he heard the mouse. \"Mouse\" said the rat, \"If you want to climb out of the basket, you must wait till you have grown as thin as you were when you went in."]
     var finishedsentence = "";
     for(var i =0;i<corpus1.length;i++)
     {
         finishedsentence += corpus1[i];
     }
+    res = finishedsentence;
     document.getElementById("demo").innerHTML = finishedsentence;
     document.getElementById("demo1").innerHTML = "Enter the number of tokens and types for the above corpus:";
     var table = document.getElementById("tab1");
@@ -49,18 +62,20 @@ function corpus1function()
     var cell11 = row2.insertCell(0);
     var cell22 = row2.insertCell(1);
     cell1.innerHTML = "#tokens:";
-    cell2.innerHTML = '<input type="text" id="token"/>'
+    cell2.innerHTML = '<input type="text" id="token" value=""/>'
     cell11.innerHTML = "#types:"
-    cell22.innerHTML = '<input type="text" id="type"/>'
+    cell22.innerHTML = '<input type="text" id="type" value=""/>'
+    var Button = '<button id="submit" onclick="comparefunc(res)">submit</button>'
+    document.getElementById("sub").innerHTML = Button;
 }
 function corpus2function()
 {
-    var corpus2 = ["A wolf carried off a lamb. The lamb said,\" I know you are going to eat me, but before you eat me I would like to hear you play the flute.","I have heard that you can play the flute better than anyone else, even the shepherd himself.", "The wolf was so pleased at this that he took out his flute and began to play.","When he had done, the lamb insisted him to play once more and the wolf played again.","The shepherd and the dogs heard the sound, and they came running up and fell on the wolf and the lamb was able to get back to the flock."]
     var finishedsentence = "";
     for(var i=0;i<corpus2.length;i++)
     {
     finishedsentence += corpus2[i];
     }
+    res = finishedsentence;
     document.getElementById("demo").innerHTML = finishedsentence;
     document.getElementById("demo1").innerHTML = "Enter the number of tokens and types for the above corpus:";
     var table = document.getElementById("tab1");
@@ -71,18 +86,20 @@ function corpus2function()
     var cell11 = row2.insertCell(0);
     var cell22 = row2.insertCell(1);
     cell1.innerHTML = "#tokens:";
-    cell2.innerHTML = '<input type="text" id="token"/>'
+    cell2.innerHTML = '<input type="text" id="token" value=" "/>'
     cell11.innerHTML = "#types:"
-    cell22.innerHTML = '<input type="text" id="type"/>'
+    cell22.innerHTML = '<input type="text" id="type" value = " "/>'
+    var Button = '<button id="submit" onclick="comparefunc(res)">submit</button>'
+    document.getElementById("sub").innerHTML = Button;
 }
 function corpus3function()
 {
-    var corpus3 = ["A man had a little dog, and he was very fond of it. He would pat its head, and take it on his knee, and talk to it.","Then he would give it little bits of food from his own plate. A donkey looked in at the window and saw the man and the dog. \"Why does he not make a pet of me?\" said the donkey.","\"It is not fair. I work hard, and the dog only wags its tail, and barks, and jumps on its master's knee. It is not fair.\" Then the donkey said to himself,","\"If I do what the dog does, he may make a pet of me.\" So the donkey ran into the room. It brayed as loudly as it could. It wagged its tail so hard that it knocked over a jar on the table.","Then it tried to jump on to its master's knee. The master thought the donkey was mad, and he shouted, \"Help! Help!\" Men came running in with sticks, and they beat the donkey till it ran out of the house,","and they drove it back to the field. \"I only did what the dog does,\" said the donkey,\" and yet they make a pet of the dog, and they beat me with sticks. It is not fair.\""]
     var finishedsentence = "";
     for(var i=0;i<corpus3.length;i++)
     {
     finishedsentence += corpus3[i];
     }
+    res = finishedsentence;
     document.getElementById("demo").innerHTML = finishedsentence;
     document.getElementById("demo1").innerHTML = "Enter the number of tokens and types for the above corpus:";
     var table = document.getElementById("tab1");
@@ -93,7 +110,50 @@ function corpus3function()
     var cell11 = row2.insertCell(0);
     var cell22 = row2.insertCell(1);
     cell1.innerHTML = "#tokens:";
-    cell2.innerHTML = '<input type="text" id="token"/>'
+    cell2.innerHTML = '<input type="text" id="tokens" value = "token">'
     cell11.innerHTML = "#types:"
-    cell22.innerHTML = '<input type="text" id="type"/>'
+    cell22.innerHTML = '<input type="text" id="types" value = "type">'
+    var Button = '<button id="submit" onclick="comparefunc(res)">submit</button>'
+    document.getElementById("sub").innerHTML = Button;
+}
+function comparefunc(res) {
+    var val = res;
+    var va = val.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, " ");
+    var val1 = va.replace(/\s{2,}/g, " ").split(" ");
+    var length1 = val1.length;
+    var ele = new Array();
+    for (var i = 0; i < val1.length; i++) {
+        var str = val1[i].toLowerCase();
+        var n = ele.includes(str);
+        if (n != true) {
+            ele.push(str);
+        }
+    }
+    var length2 = ele.length;
+    var valu1 = Number(document.getElementById("token").value);
+    var valu2 = Number(document.getElementById("type").value);
+    if (length1 == valu1 && length2 == valu2) {
+        document.getElementById("token").style.backgroundColor = "green";
+        document.getElementById("type").style.backgroundColor = "green";
+    }
+    else if (length1 == valu1 && length2 != valu2) {
+        document.getElementById("token").style.backgroundColor = "green";
+        document.getElementById("type").style.backgroundColor = "red";
+    }
+    else if (length1 != valu1 && length2 == valu2) {
+        document.getElementById("token").style.backgroundColor = "red";
+        document.getElementById("type").style.backgroundColor = "green";
+    }
+    else {
+        document.getElementById("token").style.backgroundColor = "red";
+        document.getElementById("type").style.backgroundColor = "red";
+    }
+    if (length1 == valu1 && length2 == valu2) {
+        document.getElementById("demo2").innerHTML = "Right answer";
+        var button = '<button id="continue" onclick = "rootsfunc()">continue</button>'
+        document.getElementById("root").innerHTML = button;
+    }
+    else {
+        document.getElementById("demo2").innerHTML = "wrong answer";
+    }
 }
